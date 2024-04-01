76c94663883124face7fdaaddb767d2ce3f81edf
furkan <furkan@gmail.com>
2024-04-01 21:11:36 +0600

    first commit

---
+++ fontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './navbar.scss';
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import Profileimg from "../../assets/profile.jpeg";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span>Sobji Bazar</span>
+        </Link>
+        <HomeOutlinedIcon />
+        <WbSunnyOutlinedIcon />
+        <DarkModeOutlinedIcon />
+        <GridViewOutlinedIcon />
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search..." />
+        </div>
+      </div>
+      <div className="right">
+        <PersonOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsOutlinedIcon />
+        <div className="user">
+          {/* Make sure the path to the image is correct */}
+          <img src={Profileimg} alt="profile" />
+          <span>FurKan</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
